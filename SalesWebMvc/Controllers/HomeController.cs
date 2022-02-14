a7d4f11e127374438e3fc47afe4aa82afb57e2ee
Cristian Lucas <cristian.lucasxd@gmail.com>
2022-02-14 15:38:25 -0300

    first controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Aula de .net core";
+            ViewData["aluno"] = "Aluno: Cristian Lucas";
 
             return View();
         }
