cef9a9ea84c6f1dccbde43e13d8771249169497b
Cristian Lucas <cristian.lucasxd@gmail.com>
2022-02-19 15:51:35 -0300

    Criando um cadastro simples de um vendedor

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
+
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
@@ -22,6 +24,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) //Cria o novo vendedor
+        {
+            _sellerService.Insert(seller); //Insere através do método no banco
+            return RedirectToAction(nameof(Index)); //Após isso redireciona para a view para mostrar o resultado novamente
         }
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -26,6 +26,12 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList(); //Acessa a fonte de dados de vendedores es converte para lista
         }
 
+        public void Insert(Seller obj) //Insere no banco de dados o cadastro do novo vendedor
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
 
 
     }
