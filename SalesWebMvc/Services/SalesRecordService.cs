3ae8358b8b00cd7feb4236b1286f9e067a1495f4
Cristian Lucas <cristian.lucasxd@gmail.com>
2022-02-22 23:55:49 -0300

    Implementando busca simples obs não está funcionando pois a busca está dando erro, favor Cristian do futuro resolver esta situação!

---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService : Controller
+    {
+
+        private readonly SalesWebMvcContext _context;
+
+        public SalesRecordService()
+        {
+
+        }
+
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return  result
+                .Include(x => x.seller)
+                .Include(x => x.seller.department)//Faz o join nas tabelas e lista o resultado pelo comando abaixo
+                .OrderByDescending(x => x.Date)
+                .ToList();
+
+
+        }
+    }
+}
